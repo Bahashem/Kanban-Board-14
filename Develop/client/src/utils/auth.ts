56e4eb2f49d9372773943159cbae69959cc63ce8
Diana Gaston <Bahashem@yahoo.com>
2025-02-26 10:52:37 -0500

    added authentication code

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -1,30 +1,43 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
-
+  // TODO: return the decoded token
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
-  }
-
+    const token = this.getToken();
+    return token ? jwt_decode<JwtPayload>(token) :null;
+    }
+ // TODO: return a value that indicates if the user is logged in
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
-  }
-  
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+     }
+  // TODO: return a value that indicates if the token is expired
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+  try{
+    const decoded: JwtPayload = jwt_decode(token);
+    if (decoded.exp && decoded.exp <Date.now()/1000){
+      | return true;
+    }
+  }  catch (err){
+    return false;
   }
-
+  } 
+  }
+  // TODO: return the token
   getToken(): string {
-    // TODO: return the token
+  const loggedUser = localStorage.getItem(`id_token`) || "";
+  return loggedUser;
   }
-
-  login(idToken: string) {
-    // TODO: set the token to localStorage
+  // TODO: set the token to localStorage
     // TODO: redirect to the home page
+  login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
-
-  logout() {
-    // TODO: remove the token from localStorage
+// TODO: remove the token from localStorage
     // TODO: redirect to the login page
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');  
   }
 }
 
