7436587f2fd7c71fffc7be6e4e3b3634098c1f50
Diana Gaston <Bahashem@yahoo.com>
2025-03-20 21:58:29 -0400

    added updated code

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -1,40 +1,39 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
-  // TODO: return the decoded token
+  //  return the decoded token
 class AuthService {
   getProfile() {
     const token = this.getToken();
-    return token ? jwt_decode<JwtPayload>(token) :null;
+    return token ? jwtDecode<JwtPayload>(token):null;
     }
- // TODO: return a value that indicates if the user is logged in
+ // indicates if the user is logged in
   loggedIn() {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
-     }
-  // TODO: return a value that indicates if the token is expired
+   }
+  //indicates if the token is expired
   isTokenExpired(token: string) {
   try{
-    const decoded: JwtPayload = jwt_decode(token);
+    const decoded: JwtPayload = jwtDecode(token);
     if (decoded.exp && decoded.exp <Date.now()/1000){
-      | return true;
+  return true;
     }
   }  catch (err){
     return false;
   }
   } 
-  }
-  // TODO: return the token
+  
+  //return the token
   getToken(): string {
   const loggedUser = localStorage.getItem(`id_token`) || "";
   return loggedUser;
   }
-  // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+  //  set the token to localStorage
+    // redirect to the home page
   login(idToken: string) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
-// TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+// remove the token from localStorage and redirect to the login page
   logout() {
     localStorage.removeItem('id_token');
     window.location.assign('/');  
--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -4,12 +4,9 @@ import jwt from 'jsonwebtoken';
 interface JwtPayload {
   username: string;
 }
- // TODO: verify the token exists and add the user data to the request object
+ // verify the token exists and add the user data to the request object
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+  
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
@@ -29,4 +26,4 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     res.sendStatus(401); // Unauthorized
   }
  
-};
+}
